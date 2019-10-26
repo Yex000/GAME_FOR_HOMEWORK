bd1f516d55d56256dbe6a000d4cab0124ac1924d
Pavlo Hilei <pavlikhiley@gmial.com>
2019-10-26 20:24:16 +0300

    deleted extra files
    beta2.0

--- grade_count.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-def lines():
-    surf = pygame.Surface((3, 100))
-    surf.fill((0, 0, 0)) #Solid color for lines
-
-    #line = surf.get_rect() #transform to rect
-
-    return surf
